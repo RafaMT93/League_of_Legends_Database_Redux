c6f5cb5cd44ad94867aaa83d096c253a9cace40a
RafaMT93 <rafat93.rdm@outlook.com.br>
2021-10-15 15:58:55 -0300

    CSS Reset, Theme and others basic settings added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
+import { ThemeProvider } from 'styled-components';
+import { ResetCss } from './Global/ResetCss';
+import theme from './Global/Theme';
+
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <ResetCss />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/Global/Theme.jsx
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    primary: '#fff',
+    background: '#00000014',
+    border: '#333',
+    primaryText: '#333',
+    secondatyText: '#fff',
+
+    success: '#135E01',
+  },
+  fonts: {
+    regular: 'Friz Quadrata, sans-serif',
+    fontSize: '1rem',
+  },
+};
+
+export default theme;

ab55c8e1af01f8d3ac99493fb6b7077cef503060
RafaMT93 <rafat93.rdm@outlook.com.br>
2021-10-15 16:03:48 -0300

    Routes for API added

---
+++ src/Services/API.js
@@ -0,0 +1,42 @@
+const CHAMPION_APP_URL = 'http://ddragon.leagueoflegends.com/';
+
+export function FILTER_VERSION() {
+  return {
+    url: CHAMPION_APP_URL + 'api/versions.json',
+    options: {
+      method: 'GET',
+      cache: 'no-store',
+    },
+  };
+}
+
+export function SEARCH_IN_VERSION(version) {
+  return {
+    url: CHAMPION_APP_URL + `cdn/${version}/data/pt_BR/champion.json`,
+    options: {
+      method: 'GET',
+      cache: 'no-store',
+    },
+  };
+}
+
+export function SEARCH_CHAMPION(version, champion) {
+  return {
+    url:
+      CHAMPION_APP_URL + `cdn/${version}/data/pt_BR/champion/${champion}.json`,
+    options: {
+      method: 'GET',
+      cache: 'no-store',
+    },
+  };
+}
+
+export function SEARCH_ITENS(version) {
+  return {
+    url: CHAMPION_APP_URL + `cdn/${version}/data/pt_BR/item.json`,
+    options: {
+      method: 'GET',
+      cache: 'no-store',
+    },
+  };
+}

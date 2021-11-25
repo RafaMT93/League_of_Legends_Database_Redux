a9ee7bc1e91bfd2fe3010b2c06f951895a79e945
RafaMT93 <rafat93.rdm@outlook.com.br>
2021-11-24 23:35:31 -0300

    Refactored first test of Header.test

--- src/__tests__/Header.test.jsx
+++ src/__tests__/Header.test.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from '../Components/Header';
 import '@testing-library/jest-dom';
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
 
-import { BrowserRouter } from 'react-router-dom';
-
-import { render, screen } from '@testing-library/react';
+//Styled Component Theme
 import theme from '../Global/Theme';
 import { ThemeProvider } from 'styled-components';
 
-const options = ['Champions', 'Items', 'ChampionList'];
+//Router
+import { BrowserRouter, Link } from 'react-router-dom';
+
+const options = ['First', 'Second'];
+
+afterEach(cleanup);
 
 describe('Component Header', () => {
   it('Render correctly', () => {
@@ -20,8 +24,22 @@ describe('Component Header', () => {
       </ThemeProvider>,
     );
 
-    expect(screen.queryByText(options[0])).toMatchSnapshot();
-    expect(screen.queryByText(options[1])).toMatchSnapshot();
-    expect.not.objectContaining(screen.queryByText(options[2]));
+    expect(screen.queryByText(options[0]));
+    expect(screen.queryByText(options[1]));
+    expect(screen.queryByText('Third')).not.toBeInTheDocument();
   });
+
+  /*   it('Click redirect to page', () => {
+    const { queryByText } = render(
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header options={options} />
+        </BrowserRouter>
+      </ThemeProvider>,
+    );
+    const leftClick = { button: 0 };
+    const navlink = queryByText(options[0]);
+    navlink.click(leftClick);
+    expect(screen.getByText(`/${options[0]}`));
+  }); */
 });
